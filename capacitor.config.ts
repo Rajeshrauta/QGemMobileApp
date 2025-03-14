b6ccbb76258fcb449f998cd6ae12582653d9c336
Rajesh Rauta <rajeshrauta5@gmail.com>
2025-03-14 07:08:24 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'QGemAppStandalone',
+  webDir: 'www'
+};
+
+export default config;
